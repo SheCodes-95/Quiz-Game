7bacd17f8fd3a4c57f611c81aeae27b48e9002f2
SheCodes-95 <54573209+SheCodes-95@users.noreply.github.com>
2020-02-13 13:54:24 +0200

    2nd commit
    Added effects for selecting both incorrect/correct answers

--- game.js
+++ game.js
@@ -7,36 +7,35 @@ let score=0;
 let quetionCounter= 0;
 let availableQuestions=[];
 
-let questions=[
+let questions = [
   {
     question: "Inside which HTML element do we put the JavaScript??",
     choice1: "<script>",
-    choice2: "<javascript>" ,
+    choice2: "<javascript>",
     choice3: "<js>",
     choice4: "<scripting>",
-    answer:1
+    answer: 1
   },
   {
-  question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-  choice1: "<script>",
-  choice2: "<javascript>" ,
-  choice3: "<js>",
-  choice4: "<scripting>",
-  answer:3
-
-},
-{
-question: "How do you write 'Hello World' in an alert?",
-choice1: "msgBox('Hello World');",
-choice2: "alertBox('Hello World');" ,
-choice3: "msg('Hello World');",
-choice4: "alert('Hello World');",
-answer:4
-
-}
+    question:
+      "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    choice1: "<script href='xxx.js'>",
+    choice2: "<script name='xxx.js'>",
+    choice3: "<script src='xxx.js'>",
+    choice4: "<script file='xxx.js'>",
+    answer: 3
+  },
+  {
+    question: " How do you write 'Hello World' in an alert box?",
+    choice1: "msgBox('Hello World');",
+    choice2: "alertBox('Hello World');",
+    choice3: "msg('Hello World');",
+    choice4: "alert('Hello World');",
+    answer: 4
+  }
 ];
 
-//contants
+//constants
 const CORRECT_BONUS=10;
 const MAX_QUESTIONS=3;
 
@@ -44,21 +43,24 @@ startGame= () => {
   questionCounter=0;
   score=0;
   availableQuestions=[...questions ];
-  console.log(availableQuestions);
   getNewQuestion();
 };
 
 getNewQuestion= () =>{
+
+  if(availableQuestions.length===0 || questionCounter >= MAX_QUESTIONS){
+    return window.location.assign("/end.html");
+  }
   questionCounter++;
   const questionIndex= Math.floor(Math.random() * availableQuestions.length);
   currentQuestion= availableQuestions[questionIndex];
   question.innerText=currentQuestion.question;
 
   choices.forEach(choice=> {
-    const number= choice.dataset['number'];
-    choice.innerText= currentQuestion['choice'+ number];
+    const number= choice.dataset["number"];
+    choice.innerText= currentQuestion["choice"+ number];
   
-  })
+  });
  availableQuestions.splice(questionIndex, 1);
 acceptingAnswers=true;
 
@@ -71,8 +73,18 @@ acceptingAnswers=false;
 const selectedChoice= e.target;
 const selectedAnswer = selectedChoice.dataset["number"];
 
+const classToApply ='incorrect';
+if(selectedChoice === currentQuestion.answer){
+  classToApply='correct';
+}
 
-getNewQuestion();
+selectedChoice.parentElement.classList.add(classToApply);
+setTimeout (()=> {
+  selectedChoice.parentElement.classList.remove(classToApply);
+
+  getNewQuestion();
+}, 1000);
+
 });
 
 });
